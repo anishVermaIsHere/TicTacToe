e580a9591c30d6184d4a7e3704902633c575c53c
अनीश ANiSH アニッシュ <97972189+anishVermaIsHere@users.noreply.github.com>
2022-06-04 17:02:21 +0530

    Delete TicTacToe directory

--- TicTacToe/src/App/game/components/icons.js
+++
@@ -1,7 +0,0 @@
-import {FaDotCircle} from 'react-icons/fa';
-import {ImCross} from 'react-icons/im';
-
-export const gameIcon={
-    X:<ImCross className='fa-x'/>,
-    O:<FaDotCircle className='fa-o' style={{color:'#fff'}} />
-};
